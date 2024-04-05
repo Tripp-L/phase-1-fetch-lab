cb757d4245dda5319b266c2775457bd55155647a
trippl <tlaursen8@yahoo.com>
2024-04-05 00:21:37 -0400

    Fetch Lab

--- index.js
+++ index.js
@@ -6,7 +6,22 @@ function fetchBooks() {
 
 function renderBooks(books) {
   const main = document.querySelector('main')
+  let fifthBook = books[4]
+  console.log("Fifth Book:", fifthBook.name)
+  
+  let characterIndex = 1030
+  let totalPages = books.reduce((total, book) => total + book.numberOfPages, 0)
+    console.log("Total Number of Pages:", totalPages)
+
   books.forEach(book => {
+    if (characterIndex >= 0 && characterIndex < book.characters.length) {
+      const targetCharacter = book.characters[characterIndex]
+      console.log("1031st Character:", targetCharacter)
+      characterIndex = -1 
+    } else {
+      characterIndex -= book.characters.length
+    }
+    
     const h2 = document.createElement('h2')
     h2.innerHTML = book.name
     main.appendChild(h2)
@@ -16,3 +31,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
